5eb259f4578380cea489a59af7579eec72e01073
Esteban Lundin <esteban.lunding@utem.cl>
2019-05-27 13:39:02 -0400

    added logger and removed unnecesary libraries

--- hgtDrawer.py
+++ hgtDrawer.py
@@ -1,18 +1,11 @@
 import logging
-import fire
-import os
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-from matplotlib import cm
-from tqdm import tqdm
-from matplotlib.colors import LightSource
 from logging.config import fileConfig
 from gmalthgtparser import HgtParser
-from PIL import Image
 
-# hgt_file = 'hgt_files/N00W050.hgt'
 
 fileConfig('logging_config.ini')
 logger = logging.getLogger(__name__)
@@ -26,10 +19,10 @@ class HgtDrawer(object):
         self.altitude_angle = altitude_angle
 
     def get_elevation(self):
+        logger.info("Obteniendo arreglo de elevacion...")
         with HgtParser(self.hgt_file) as parser:
             for elevation in parser.get_sample_iterator(1201, 1201):
                 elevation_array = np.array(elevation[4])
-
         self.get_image(
             elevation_array=elevation_array,
             azimuth_angle=self.azimuth_angle,
@@ -38,25 +31,23 @@ class HgtDrawer(object):
 
     @staticmethod
     def get_image(elevation_array, azimuth_angle, altitude_angle):
+        logger.info("Renderizando imagen...")
         x, y = np.gradient(elevation_array)
-        slope = np.pi/2. - np.arctan(np.sqrt(x*x + y*y))
+        pendant = np.pi/2. - np.arctan(np.sqrt(x*x + y*y))
         aspect = np.arctan2(-x, y)
+
         azimuth_rad = azimuth_angle*np.pi / 180.
         altitude_rad = altitude_angle*np.pi / 180.
-        shaded = np.sin(altitude_rad) * np.sin(slope) + np.cos(altitude_rad) * np.cos(slope) * np.cos(azimuth_rad - aspect)
-
+        shaded = np.sin(altitude_rad) * np.sin(pendant) + np.cos(altitude_rad) * np.cos(pendant) * np.cos(azimuth_rad - aspect)
         value = 255*(shaded + 1)/2
-
         plt.imshow(value, cmap='Greys')
         plt.show()
 
 
 if __name__ == '__main__':
     drawer = HgtDrawer(
-        hgt_file='hgt_files/N00W050.hgt',
+        hgt_file='hgt_files/S33W070.hgt',
         azimuth_angle=315,
-        altitude_angle=15,
+        altitude_angle=45,
     )
     drawer.get_elevation()
-    fire.Fire(HgtDrawer)
-
